151d977668f74225ef9e4e8654c758f70dd3e639
Mirko van der Waal <mvdw@airmail.cc>
2015-10-17 16:28:01 +0200

    Thanks github

--- kawaiibot/models/bot.py
+++ kawaiibot/models/bot.py
@@ -1,16 +1,21 @@
 import os
 import sys
-import telegram
-import json
 
+try:
+    import telegram
+    import json
+except ImportError as e:
+    print('You\'ve failed to import one of the required modules. Did you pip the requirements? {}'.format(e), file=sys.stderr)
+    sys.exit(1)
 try:
     with open(os.path.expanduser('~/.config/kawaiibot.json')) as f:
         config = json.load(f)
-except FileNotFoundError:
-    print('Config file not found. Does ~/.config/kawaiibot.json exist?', file=sys.stderr)
+except FileNotFoundError as e:
+    print('The require configuration file was not found. Are you sure that \'~/.config/kawaiibot.json\' exist? {}'.format(e), file=sys.stderr)
     sys.exit(1)
 
 class Bot():
+    logfile = os.path.expanduser('~/kawaiibot.log')
     commands = {}
     last_id = None
 
@@ -18,29 +23,37 @@ class Bot():
         self.__dict__ = attributes
         self.prefix = prefix
 
-    def log(self, to, message, status):
+    def log(self, message, status):
         """ Create a new log entry.
-            @to: The logfile to write to.
             @message: A brief explanation regarding the log entry.
-            @status: Success, failure, warning.
+            @status: 0: Success, 1: Warning, 2: Error.
         """
 
-        with open(to, 'a') as f:
+        with open(self.logfile, 'a') as f:
             f.write('{} [{}]'.format(message, status))
+
+        if status in [2, 3]:
+            print(message, file=sys.stderr)
+        print(message, file=sys.stdout)
 
     def command(self, handle):
         """ Create a new command entry, saved in self.commands
             @handle: The unique handle for the command call.
         """
 
+        self.log('Attempting to create new command', 0)
         def arguments(function):
             if type(function).__name__ == 'function':
                 self.commands[handle] = function
+            else:
+                self.log('{}.{} -> {}'.format(self.__class__.__name__, '__name__',
+                         'Detected argument was not a function'), 2)
+            self.log('Succesfully created a new command -> {}'.format(handle), 0)
             return self.commands
         return arguments
 
     def run(self):
-        """ Initially start listening for chat events."""
+        """ Initially start listening for chat events. """
         bot = telegram.Bot(token=config['telegram']['token'])
 
         # TODO: Make this not hacky with tries.
