bfc7077af661ad9cc68c2cc99617546d62b58ccc
Steven Oud <soud@protonmail.com>
2015-10-19 18:44:09 +0200

    telegram has built-in function for showing all commands - so we dont need that anymore

--- kawaiibot/commands/misc.py
+++ kawaiibot/commands/misc.py
@@ -1,5 +1,11 @@
 from kawaiibot.bot import kawaiibot
 
+@kawaiibot.command('start')
+def start(args):
+    # This needs working on, telegram automatically issues this command when
+    # you talk to the bot for the first time.
+    return 'This bot searches Imgur and DuckDuckGo. It also has some other fun usages.'
+
 @kawaiibot.command('hey')
 def hey(args):
     return 'Hi!'
