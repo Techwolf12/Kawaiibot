bda6758b21b3ed3d00b0e9a81737ad865e4c5e02
Steven Oud <soud@protonmail.com>
2015-10-19 18:34:49 +0200

    reformatted commands

--- kawaiibot/commands/telegram.py
+++ kawaiibot/commands/telegram.py
@@ -2,9 +2,16 @@ from kawaiibot.bot import kawaiibot
 
 @kawaiibot.command('start')
 def start(args):
-    # This needs working on, telegram automatically issues this command when you talk to the bot for the first time.
-    return 'This bot searches Imgur and Duckduckgo. It also has some other fun usages.'
+    # This needs working on, telegram automatically issues this command when
+    # you talk to the bot for the first time.
+    return 'This bot searches Imgur and DuckDuckGo. It also has some other fun usages. \
+See /commands for everything I can do!'
 
 @kawaiibot.command('commands')
 def commands(args):
-    return 'Imgur: /r DuckDuckGo: /ddg Tableflip: /flip Table back: /back Kaf image: /kaf'
+    return '''Imgur: /r <sub>\n
+DuckDuckGo: /ddg <query>\n
+Tableflip: /flip\n
+Table back: /back\n
+Kaf image: /kaf\n
+DuckDuckGo bang: /!<bang> <query>'''
